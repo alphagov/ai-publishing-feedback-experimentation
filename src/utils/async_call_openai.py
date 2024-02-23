87d57cd66db94be63e1e5ceb2646a0e7adc8af63
Henry Wrightson <henry.wrightson@digital.cabinet-office.gov.uk>
2024-02-23 15:43:36 +0000

    feat: update prompt and call in notebook to make 'urgency' a consistent name to avoid inconsistent outputs

--- src/utils/async_call_openai.py
+++ src/utils/async_call_openai.py
@@ -8,23 +8,22 @@ async def create_openai_labelled_data(
     new_example: str,
     open_api_key: str,
 ) -> dict:
-
     client = AsyncOpenAI(api_key=open_api_key)
 
-    system_prompt = f"""
+    system_prompt = """
         You are an expert tasked with categorising user feedback for the UK government, submitted through the website www.gov.uk. Your input is a JSON containing two key pieces of information: a unique identifier (id) and the user feedback (feedback). Your objective is to analyze the feedback and assign an appropriate label or labels that accurately categorise the feedback. Every piece of feedback must receive at least one label. In instances where the feedback is irrelevant or does not pertain to government services (e.g., promotional content, unrelated comments), it should be categorised as "Spam".
 
-        Additionally, you must assess the urgency of the feedback on a scale from 1 to 5, where 1 signifies feedback that is not urgent and 5 denotes feedback requiring immediate attention. This urgency rating should reflect the extent to which the feedback suggests that prompt action is necessary to address any issues with a government digital service.
+        Additionally, you must assess the urgency of the feedback on a scale from 1 to 5, where 1 signifies feedback that is not urgent and 5 denotes feedback requiring immediate attention. This urgency should reflect the extent to which the feedback suggests that prompt action is necessary to address any issues with a government digital service.
 
         It is crucial to process this information thoughtfully, considering the potential impact of the feedback on public services and the well-being of citizens. For example, feedback reporting a broken link on a page providing vital health information would be considered more urgent than feedback suggesting a minor cosmetic change to the website’s homepage.
 
-        Your output must be in valid JSON format, including only the id, the assigned labels, and the urgency rating for each piece of feedback. This structured approach ensures clarity and consistency in how feedback is processed and prioritised.
+        Your output must be in valid JSON format, including only the id, the assigned labels, and the urgency for each piece of feedback. This structured approach ensures clarity and consistency in how feedback is processed and prioritised.
 
         Remember, your analysis and categorisation play a vital role in improving government digital services and ensuring that they meet the needs of the public efficiently and effectively.
         """
 
     user_prompt = f"""
-        Before you label the following record, let's reflect on the examples provided and apply similar reasoning to ensure consistency and accuracy in our categorisation. Consider the nature of the feedback, its relevance to government services, and the immediacy with which the issue it raises should be addressed. This thoughtful approach will guide you in determining the most appropriate labels and the urgency rating.
+        Before you label the following record, let's reflect on the examples provided and apply similar reasoning to ensure consistency and accuracy in our categorisation. Consider the nature of the feedback, its relevance to government services, and the immediacy with which the issue it raises should be addressed. This thoughtful approach will guide you in determining the most appropriate labels and the urgency.
 
         Here are the examples for reference:
         {labelled_examples}
@@ -32,7 +31,7 @@ async def create_openai_labelled_data(
         Based on these examples, let's proceed to categorise the new piece of feedback. Remember, we are focusing on identifying the most fitting labels and assessing the urgency accurately, all while ensuring our output is in valid JSON format. Here's the feedback you need to label:
         {new_example}
 
-        Your output should include only the id, the labels, and the urgency rating. Reflect on the content of the feedback, its implications for government services, and the potential impact on users to make your assessment. Always return your analysis in valid JSON format.
+        Your output should include only the keys "id", "labels", and "urgency", and their respective values. Reflect on the content of the feedback, its implications for government services, and the potential impact on users to make your assessment. Always return your analysis in valid JSON format.
     """
 
     try:
