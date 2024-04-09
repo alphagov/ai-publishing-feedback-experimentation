04eeb0331f8289578fd0978940caece6d1f6c6e5
Henry Wrightson <henry.wrightson@digital.cabinet-office.gov.uk>
2024-04-09 16:46:37 +0100

    notebook: test eval in notebook

--- evaluation/run_evaluation.py
+++ evaluation/run_evaluation.py
@@ -6,17 +6,21 @@ from src.collection.evaluate_collection import (
     assess_scroll_retrieval,
 )
 from src.utils.utils import load_qdrant_client, load_config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Get env vars
 PUBLISHING_PROJECT_ID = os.getenv("PUBLISHING_PROJECT_ID")
 EVALUATION_TABLE = os.getenv("EVALUATION_TABLE")
+EVALUATION_TABLE = f"`{EVALUATION_TABLE}`"
 QDRANT_HOST = os.getenv("QDRANT_HOST")
 QDRANT_PORT = os.getenv("QDRANT_PORT")
-EVAL_COLLECTION_NAME = os.getenv("EVAL_COLLECTION_NAME")
+EVAL_COLLECTION_NAME = os.getenv("COLLECTION_NAME")
 HF_MODEL_NAME = os.getenv("HF_MODEL_NAME")
 
 config = load_config(".config/config.json")
-similarity_threshold = float(config.get("dot_product_threshold_1"))
+similarity_threshold = float(config.get("similarity_threshold_1"))
 print(f"Similarity threshold: {similarity_threshold}")
 
 
@@ -30,14 +34,11 @@ def main():
         evaluation_table=EVALUATION_TABLE,
     )
 
-    # Read data_regex_counts.pkl
-    with open("data/regex_counts.pkl", "rb") as f:
-        regex_counts = pickle.load(f)
-
+    # Load the regex ids
     with open("data/regex_ids.pkl", "rb") as f:
         regex_ids = pickle.load(f)
 
-    # Assess the retrieval accuracy
+    # Assess the retrieval accuracy, return result ids
     ss_results = assess_retrieval_accuracy(
         client=client,
         collection_name=EVAL_COLLECTION_NAME,
@@ -48,7 +49,7 @@ def main():
     )
     print(f"Dot product search n results: {len(ss_results)}")
 
-    # Assess the scroll retrieval accuracy
+    # Assess the scroll retrieval accuracy return result ids
     scroll_results = assess_scroll_retrieval(
         client=client,
         collection_name=EVAL_COLLECTION_NAME,
@@ -56,6 +57,14 @@ def main():
         regex_ids=regex_ids,
     )
     print(f"Scroll n results: {len(scroll_results)}")
+
+    # Join the results together and output to csv
+    results = {
+        "regex_results": regex_ids,
+        "cosine_results": ss_results,
+        "scroll_results": scroll_results,
+    }
+    print(results)
 
 
 if __name__ == "__main__":
