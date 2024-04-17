bb3ea9f01e438d4f9c52f8e2f7ab47c6809ac31f
Henry Wrightson <henry.wrightson@digital.cabinet-office.gov.uk>
2024-04-17 13:17:42 +0100

    refactor: cache file checking/read in to avoid rerunning when app refreshes

--- evaluation/app.py
+++ evaluation/app.py
@@ -4,7 +4,7 @@ import pickle
 import streamlit as st
 import subprocess
 import plotly.graph_objs as go
-from src.collection.evaluate_collection import (
+from src.collection_utils.evaluate_collection import (
     create_precision_boxplot_data,
     create_recall_boxplot_data,
     create_precision_line_data,
@@ -21,32 +21,53 @@ PUBLISHING_PROJECT_ID = os.getenv("PUBLISHING_PROJECT_ID")
 EVALUATION_TABLE = os.getenv("EVALUATION_TABLE")
 EVALUATION_TABLE = f"`{EVALUATION_TABLE}`"
 
-# Check if the pickle files exist
-if (
-    not os.path.exists("data/regex_ids.pkl")
-    or not os.path.exists("data/precision_values.pkl")
-    or not os.path.exists("data/recall_values.pkl")
-):
-    # run the evaluation/create_eval_json.py script
+
+# Check if the pickle files exist via a cached function
+@st.cache_data
+def check_pickle_files():
+    data_path = "data"
+    required_files = [
+        "regex_ids.pkl",
+        "precision_values_async.pkl",
+        "recall_values_async.pkl",
+        "f2_scores_async.pkl",
+    ]
+    files_exist = os.path.isdir(data_path) and all(
+        os.path.exists(os.path.join(data_path, file)) for file in required_files
+    )
+    return files_exist
+
+
+# Create the pickle files via a cached function
+@st.cache
+def create_files():
     print("Running evaluation/create_eval_json.py ...")
     subprocess.run(
         ["python", "evaluation/create_eval_json.py", "--save_outputs", "True"]
     )
 
-# Load the data
-with open("data/regex_ids.pkl", "rb") as f:
-    regex_ids = pickle.load(f)
 
-with open("data/precision_values_test.pkl", "rb") as f:
-    precision_values = pickle.load(f)
-
-with open("data/recall_values_test.pkl", "rb") as f:
-    recall_values = pickle.load(f)
-
-with open("data/f2_scores_test.pkl", "rb") as f:
-    f2scores = pickle.load(f)
+# Check if the pickle files exists
+if not check_pickle_files():
+    create_files()
 
 
+# Load the pickle files via a cached function
+@st.cache
+def load_pickle_data(file_path):
+    with open(file_path, "rb") as file:
+        data = pickle.load(file)
+    return data
+
+
+# Load the pickle files
+# TODO: can I parameterise the list of files so I don't type here and in the check_pickle_files function?
+regex_ids = load_pickle_data("data/regex_ids.pkl")  # TODO: Do I need this?
+recall_values = load_pickle_data("data/recall_values_async.pkl")
+precision_values = load_pickle_data("data/precision_values_async.pkl")
+f2scores = load_pickle_data("data/f2_scores_async.pkl")
+
+# Create the data for the box plots and line plots
 precision_boxplot_data = create_precision_boxplot_data(precision_values)
 recall_boxplot_data = create_recall_boxplot_data(recall_values)
 precision_line_data = create_precision_line_data(precision_values)
@@ -63,18 +84,19 @@ def main():
         "Select Threshold", min_value=0.0, max_value=1.0, step=0.1
     )
 
+    # Get the precision, recall, and f2 scores as lists
     thresholds = list(precision_line_data.keys())
     precision_scores = list(precision_line_data.values())
     recall_scores = list(recall_line_data.values())
     f2scores = list(f2scores_line_data.values())
 
-    # Find the precision score for the selected threshold
-    selected_precision_index = thresholds.index(selected_threshold)
-    selected_precision = precision_scores[selected_precision_index]
-    selected_recall = recall_scores[selected_precision_index]
-    selected_f2 = f2scores[selected_precision_index]
+    # Index the score lists for the selected threshold
+    selected_index = thresholds.index(selected_threshold)
+    selected_precision = precision_scores[selected_index]
+    selected_recall = recall_scores[selected_index]
+    selected_f2 = f2scores[selected_index]
 
-    # Display the selected precision score
+    # Display the selected scores as metrics
     col1, col2, col3 = st.columns(3)
     with col1:
         st.metric("Precision Score", selected_precision)
@@ -86,7 +108,7 @@ def main():
     # Plot
     fig = go.Figure()
 
-    # Add line plot for thresholds vs. precision scores
+    # Add line plot traces for thresholds vs. scores
     fig.add_trace(
         go.Scatter(x=thresholds, y=precision_scores, mode="lines", name="Precision")
     )
