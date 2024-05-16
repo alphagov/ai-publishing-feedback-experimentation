ead192868c196560e2fc216484adaadc4c9a624d
Richard Eveson <richard.eveson@digital.cabinet-office.gov.uk>
2024-05-16 17:24:49 +0100

    feat: add scoll search to test script

--- collection/test.py
+++ collection/test.py
@@ -2,10 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from qdrant_client import QdrantClient
+from qdrant_client.http.models import FieldCondition, Filter, MatchAny
 
 
 load_dotenv()
-QDRANT_HOST = os.getenv("QDRANT_HOST_EXTERNAL")
+QDRANT_HOST = os.getenv("QDRANT_HOST")
 QDRANT_PORT = os.getenv("QDRANT_PORT")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME")
 EVAL_COLLECTION_NAME = os.getenv("EVAL_COLLECTION_NAME")
@@ -17,3 +18,20 @@ print(COLLECTION_NAME[:2])
 client = QdrantClient(QDRANT_HOST, port=QDRANT_PORT)
 
 print(client.get_collections())
+
+
+filter_dict = {"primary_department": ["Government Digital Service"]}
+filter = Filter(
+    must=[
+        FieldCondition(key=filter_key, match=MatchAny(any=filter_values))
+        for filter_key, filter_values in filter_dict.items()
+        if filter_values
+    ]
+)
+
+test_results = client.scroll(
+    collection_name=COLLECTION_NAME,
+    scroll_filter=filter,
+    limit=10,
+)
+print(len(test_results))
